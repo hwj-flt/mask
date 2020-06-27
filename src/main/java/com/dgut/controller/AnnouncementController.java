89279faaf49fc9d9a25eab184a567bf02bfc5195
马铭洪 <1151784641@qq.com>
2020-06-27 21:13:30 +0800

    公告栏功能修改

--- src/main/java/com/dgut/controller/AnnouncementController.java
+++ src/main/java/com/dgut/controller/AnnouncementController.java
@@ -1,14 +1,23 @@
 package com.dgut.controller;
 
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import com.dgut.domain.Announcement;
+import com.dgut.domain.Result;
+import com.dgut.domain.ResultStatus;
 import com.dgut.service.AnnouncementService;
+import com.google.gson.Gson;
 import org.apache.ibatis.annotations.Insert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
@@ -24,27 +33,35 @@ public class AnnouncementController {
     }
 
     @RequestMapping("/insert")
-    public String insert(@RequestParam(value = "num",required = false)Integer num,@RequestParam(value = "content",required = false)String content){
+    @ResponseBody
+    @CrossOrigin
+    public Result insert(HttpServletRequest request){
         Announcement a =new Announcement();
         Calendar calendar = Calendar.getInstance();
         Date time=calendar.getTime();
-        a.setContent(content);
-        a.setNum(num);
+        a.setContent(request.getParameter("content"));
+        a.setNum(Integer.valueOf(request.getParameter("num")));
         a.setTime(time);
         announcementService.insert(a);
-        return "announcement";
+        return new Result(ResultStatus.SUCCESS);
     }
-    @RequestMapping("/show")
-    public String show(){
+    @PostMapping("/show")
+    @ResponseBody
+    @CrossOrigin
+    public Result show(HttpSession session){
         Announcement a =new Announcement();
         a=announcementService.show();
-        System.out.println(a);
-        return "announcement";
+        JSONObject jsonObject= (JSONObject) JSONObject.toJSON(a);
+        System.out.println(jsonObject.toString());
+
+        return new Result(ResultStatus.SUCCESS,jsonObject);
     }
     @RequestMapping("showall")
-    public String showall(){
+    @ResponseBody
+    @CrossOrigin
+    public Result showall(){
         List<Announcement> a= announcementService.showall();
-        System.out.println(a);
-        return "announcement";
+        JSONArray jsonArray = (JSONArray) JSONArray.toJSON(a);
+        return new Result(ResultStatus.SUCCESS,jsonArray);
     }
 }
