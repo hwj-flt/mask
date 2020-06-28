6f6830dc0393a89916455e81e24a463f78621ea0
huangweijian_fellett <535363313@qq.com>
2020-06-28 17:58:33 +0800

    整合long

--- src/main/java/com/dgut/mapper/UserMapper.java
+++ src/main/java/com/dgut/mapper/UserMapper.java
@@ -1,11 +1,7 @@
 package com.dgut.mapper;
 
 import com.dgut.domain.User;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Delete;
-import org.apache.ibatis.annotations.Select;
-import org.apache.ibatis.annotations.Update;
+import org.apache.ibatis.annotations.*;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 
@@ -21,11 +17,7 @@ public interface UserMapper {
      */
     @Select("select * from user")
     public List<User> findAllUser();
-    /*
-    根据用户名查找用户
-     */
-    @Select("select * from user where username=#{username}")
-    public User findUserByUsername(String username);
+
     /*
     用户注册成功，增加用户信息
      */
@@ -41,4 +33,24 @@ public interface UserMapper {
      */
     @Update("update user set username=#{username},name=#{name},sex=#{sex},password=#{password},phone=#{phone},role=#{role},status=#{status} where id=#{id}")
     public int updateUserById(User user);
+
+    /*
+    根据用户名查找用户
+     */
+    @Select("select * from user LEFT JOIN `order` ON order.id=user.id where username=#{username}")
+    @Results(value = {@Result(property = "id",column = "id"),@Result(property = "order.id",column = "id"),
+            @Result(property = "order.state",column = "state")})
+    User findUserByUsername(String username);
+    /*
+    根据用户名修改用户信息,role用户不能更改,管理员可以更改，用户进行口罩预约时可改status为1，执行操作前将不改的变量设置为null
+     */
+    @Update("<script>update user set name=#{name},sex=#{sex},password=#{password},id=#{id},birthday=#{birthday},address=#{address}," +
+            "phone=#{phone}<if test='status != null '>,status=#{status}</if>" +
+            "<if test='role != null '>,status=#{status}</if> where username=#{username}</script>")
+    int updateUserByUsername(User user);
+    /*
+    预约成功后插入一条order.state=0 的数据
+    */
+    @Insert("Insert into `order` values(#{id},0)")
+    int insertOrder(String id);
 }
